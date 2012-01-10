cdac456b4130c3a93ad4a57deb4f6b962bc9c941
nuomi <newchar@gmail.com>
2012-01-10 12:46:07 +0800

    adopt jekyll

---
+++ js/artlife.js
@@ -0,0 +1,80 @@
+$(document).ready(function(){
+    $("#nav-artclass").click(function(){
+        $("#main").load("artclass.html");
+    });
+
+    $("#nav-artjam").click(function(){
+        $("#main").load("artjam.html");
+    });
+
+    $("#nav-originalart").click(function(){
+        $("#main").load("originalart.html");
+    });
+
+    $("#nav-homedecor").click(function(){
+        $("#main").load("homedecor.html");
+    });
+
+    $("#nav-wearableart").click(function(){
+        $("#main").load("wearableart.html");
+    });
+
+    $("#nav-privateparties").click(function(){
+        $("#main").load("privateparties.html");
+    });
+
+    $("#nav-menu-homepage").click(function(){
+        $("#main").load("homepage.html");
+    });
+
+    $("#nav-menu-contact").click(function(){
+        $("#main").load("contact.html");
+    });
+
+    $("#nav-menu-map").click(function(){
+        $("#main").load("map.html");
+    });
+
+    $("#studentworks").click(function(){
+        $("#main").load("studentworks.html");
+    });
+
+    $("#footer-artclass").click(function(){
+        $("#main").load("artclass.html")
+    });
+
+    $("#footer-artjam").click(function(){
+        $("#main").load("artjam.html")
+    });
+
+    $("#footer-originalart").click(function(){
+        $("#main").load("originalart.html")
+    });
+
+    $("#footer-homedecor").click(function(){
+        $("#main").load("homedecor.html")
+    });
+
+    $("#footer-wearableart").click(function(){
+        $("#main").load("wearableart.html")
+    });
+
+    $("#footer-privateparties").click(function(){
+        $("#main").load("privateparties.html")
+    });
+
+    $("#footer-homepage").click(function(){
+        $("#main").load("homepage.html")
+    });
+
+    $("#footer-contacts").click(function(){
+        $("#main").load("contact.html")
+    });
+
+    $("#footer-map").click(function(){
+        $("#main").load("map.html")
+    });
+
+    $("#main").load("homepage.html");
+
+});
